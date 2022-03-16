32bed45929fdd018a711994dc6c7569e9921a0ed
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-03-16 10:47:59 +0100

    done

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import { RequestBody } from "./types";
 
 
 // FINISH-ME: set to your server's endpoint
-export const host = ``;
+export const host = `http://localhost:4001`;
 
 export const sendRequest = async (endpoint: string, method: string, bodyParam?: RequestBody, token?: string) => {
   const headers: any = {
@@ -11,6 +11,7 @@ export const sendRequest = async (endpoint: string, method: string, bodyParam?: 
   };
 
   if (token) headers.authorization = token;
+  console.log(token)
 
   const body = bodyParam ? JSON.stringify(bodyParam) : undefined
 
@@ -25,30 +26,31 @@ export const sendRequest = async (endpoint: string, method: string, bodyParam?: 
 
 export const handleLogout = async () => {
   //FINISH-ME: remove token from local storage 
-
+  localStorage.clear()
   window.location.href = '/';
 };
 
 export const handleSignUp = async (body: RequestBody) => {
-  await sendRequest('signup', 'POST', body);
+  await sendRequest('sign-up', 'POST', body);
   window.location.href = '/';
 }
 
 export const handleLogin = async (body: RequestBody) => {
-  const result = await sendRequest('login', 'POST', body);
+  const result = await sendRequest('sign-in', 'POST', body);
 
   const { data, token, error } = result;
   // FINISH-ME: set token in local storage
-
+  localStorage.token = token;
   return { data, error }
 }
 
 export const signInWithJWT = async () => {
 
-  let token; // FINISH-ME: check for token
+  const token = localStorage.getItem('token');
+  if (!token || token === "undefined") return; // FINISH-ME: check for token
   // Note: if token is not found, it can be 'undefined'(string)
 
   const result = await sendRequest('banking-info', 'GET', undefined, token);
-
+  console.log(result);
   return result || null;
 }
